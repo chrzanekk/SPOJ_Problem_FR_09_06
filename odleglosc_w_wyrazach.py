0925ca2f6b178576445830e7200b495a2f9d8ff6
Konrad <chrzanekk@gmail.com>
2018-10-29 18:49:16 +0100

    Update odleglosc_w_wyrazach.py
    Final version accepted by polish SPOJ

--- odleglosc_w_wyrazach.py
+++ odleglosc_w_wyrazach.py
@@ -1,45 +1,20 @@
 import string
 
-wyrazy = []
+words = []
 alpha = list(string.ascii_uppercase)
-list_of_word_indexes = []
-ile_wyrazow = int(input("Podaj ilosc wyrazow: "))
-#print(alpha)
-#print(len(alpha))
-for i in range(ile_wyrazow):
-    temp_wyraz = input("Podaj wyraz: ")
-    if len(temp_wyraz)>=2 and len(temp_wyraz)<= 1000:
-        wyrazy.append(temp_wyraz.upper())
-    else:
-        continue
-
-
-for word in wyrazy:
+how_many_words = int(input())
+for i in range(how_many_words):
+    word = str(input())
+    word = word.upper()
     list_of_indexes = []
     for letter in word:
         if letter in alpha:
-            list_of_indexes.append(alpha.index(letter))
-    list_of_word_indexes.append(list_of_indexes)
-
-
-for list in list_of_word_indexes:
-    min_value = min(list)
-    max_value = max(list)
-    difference = max_value - min_value
+            letter_index = alpha.index(letter)
+            list_of_indexes.append(letter_index)
+    difference = max(list_of_indexes) - min(list_of_indexes)
     print(difference)
 
 
-#min_value = min(list_of_indexes)
-#max_value = max(list_of_indexes)
-#difference = max_value - min_value
-#print(min_value)
-#print(max_value)
-#print(difference)
-
-
-
-            
     
-
-
+    
 
